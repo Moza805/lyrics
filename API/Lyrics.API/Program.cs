e0c067cb6aa93d46813b6fdb43e272add81eed70
Jack Thorpe <jackthorpe1@hotmail.com>
2022-02-14 18:58:36 +0000

    Add application insights

--- API/Lyrics.API/Program.cs
+++ API/Lyrics.API/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
